3b91c58ed3e2718303aafd8b0eadedfb174487fe
SQUARED <141570367+SQUARED0@users.noreply.github.com>
2024-02-18 02:17:49 +0200

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,28 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event161_Act2(runtime, localVars)
+	{
+		
+		var seconds = runtime.globalVars.GameTime; 
+		
+		
+		var minutes = Math.floor(seconds / 60);
+		var remainingSeconds = seconds % 60;
+		
+		
+		var formattedSeconds = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+		
+		
+		var formattedTime = minutes + ':' + formattedSeconds;
+		
+		
+		runtime.globalVars.FormateTime = formattedTime;
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
